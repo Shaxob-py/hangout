28b9a213b826f49cac950205e9097e735b49adaa
Shaxobff <xunuddinovshaxob@gmail.com>
2026-05-31 23:43:07 +0500

    update

--- admin/event.py
+++ admin/event.py
@@ -1,6 +1,6 @@
 from starlette_admin.contrib.sqla import ModelView
 from starlette_admin.contrib.sqla import ModelView
-from starlette_admin.fields import StringField, IntegerField, EnumField, DateTimeField , UUIDField
+from starlette_admin.fields import StringField, IntegerField, EnumField, DateTimeField
 
 
 
@@ -9,7 +9,7 @@ class EventsModelView(ModelView):
     fields = [
         StringField("name", label="Name", help_text="togri yoz"),
         StringField("description", label="Description"),
-        UUIDField("owner_id", label="Owner id"),
+        StringField("owner_id", label="Owner id"),
         StringField("location", label="Location"),
         DateTimeField("date_event", label="Date Event"),
     ]
--- admin/users.py
+++ admin/users.py
@@ -1,4 +1,4 @@
-from starlette_admin import IntegerField, EnumField, StringField , UUIDField
+from starlette_admin import IntegerField, EnumField, StringField
 from starlette_admin.contrib.sqla import ModelView
 
 from database import User
@@ -6,7 +6,7 @@ from database import User
 
 class UserModelView(ModelView):
     fields = [
-        UUIDField("id", label="ID"),
+        StringField("id", label="ID"),
         StringField("username", label="Username", help_text="togri yoz"),
         StringField("phone", label="Phone"),
         IntegerField("telegram_id", label="Telegram ID"),
